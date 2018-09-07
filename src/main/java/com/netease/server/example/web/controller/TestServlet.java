580ef3b74deab5d081d2bfd320dea1212b52f39d
123 <1107540794@qq.com>
2018-09-07 15:58:13 +0800

    提交servletContext.getResource课程

--- src/main/java/com/netease/server/example/web/controller/TestServlet.java
+++ src/main/java/com/netease/server/example/web/controller/TestServlet.java
@@ -1,7 +1,10 @@
 package com.netease.server.example.web.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.netease.server.example.util.GeneralUtil;
+
+import java.io.*;
+import java.net.URL;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +16,14 @@ public class TestServlet extends HttpServlet {
 
 	@Override
 	public void init(){
+		//获取配置文件中全局参数
+		ServletContext context = this.getServletContext();
+		System.out.println(context.getInitParameter("contextParam1"));
+		System.out.println(context.getInitParameter("contextParam2"));
 		System.out.println("init method");
+		//获取loginServlet中的动态设置的参数
+		System.out.println("helloServlet中："+context.getAttribute("account"));
+		System.out.println("helloServlet中："+context.getAttribute("password"));
 	}
 
 	@Override
@@ -35,10 +45,29 @@ public class TestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		System.out.println("doGet method");
-		PrintWriter pw = resp.getWriter();
-		pw.print("/hello");
-		pw.close();
+		ServletContext context = this.getServletContext();
+		//获取外部文件方法联系（getResource\getResourceAsStream\getRealPath
+		//首先通过getResource获取外部资源
+		//path对象应该注意相对web的路径
+		URL url = context.getResource("/WEB-INF/classes/log4j.properties");
+		//拿到文件路径之后获取文件流，
+		InputStream inputStream = url.openStream();
+		//通过预写好的工具类获取properties里面的某参数对应的value
+		String log4jRootLogger = GeneralUtil.getPropery("log4j.rootLogger",inputStream);
+		System.out.println("getResource方法获取的properties文件内的对象log4j.rootLogger的内容为：/n"+log4jRootLogger);
+		//通过getResourceAsSteam方法获取外部资源（对比上一步，节省了转换的步骤
+		InputStream inputStream1 = context.getResourceAsStream("/WEB-INF/classes/log4j.properties");
+		String log4jRootLogger1 = GeneralUtil.getPropery("log4j.rootLogger",inputStream1);
+		System.out.println("getResourceAsSteam方法获取的properties文件内的对象log4j.rootLogger的内容为：/n"+log4jRootLogger1);
+		//通过getRealPath方法获取外部资源
+		String path = context.getRealPath("/WEB-INF/classes/log4j.properties");
+		System.out.println(path);
+		//通过绝对路径获取file对象
+		File file = new File(path);
+		InputStream inputStream2 = new FileInputStream(file);
+		String log4jRootLogger2 = GeneralUtil.getPropery("log4j.rootLogger",inputStream2);
+		System.out.println("getRealPath方法获取的properties文件内的对象log4j.rootLogger的内容为：/n"+log4jRootLogger2);
+
 	}
 
 }
--- src/main/java/com/netease/server/example/web/controller/login/LoginServlet.java
+++ src/main/java/com/netease/server/example/web/controller/login/LoginServlet.java
@@ -1,11 +1,15 @@
 package com.netease.server.example.web.controller.login;
 
+import com.netease.server.example.util.GeneralUtil;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URL;
 
 /**
  * @description: <文件描述>
@@ -16,10 +20,24 @@ import java.io.PrintWriter;
  */
 public class LoginServlet extends HttpServlet{
 
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //配置文件中全局静态context参数获取
+        ServletContext context = this.getServletContext();
+        System.out.println("静态context参数"+context.getInitParameter("contextParam1"));
+        System.out.println("静态context参数"+context.getInitParameter("contextParam2"));
+        //servlet内部参数获取
+        ServletConfig servletConfig = this.getServletConfig();
+        System.out.println("servlet内部参数"+servletConfig.getInitParameter("data1"));
+        System.out.println("servlet内部参数"+servletConfig.getInitParameter("data2"));
         String account = req.getParameter("account");
         String password = req.getParameter("password");
+        //动态设置全局参数(在TestServlet内写了获取的方法）
+        context.setAttribute("account",account);
+        context.setAttribute("password",password);
+        //打印出页面数据
         PrintWriter pw = resp.getWriter();
         pw.println("account:"+account);
         pw.println("password:"+password);
@@ -28,6 +46,7 @@ public class LoginServlet extends HttpServlet{
         }else {
             pw.println("login fail");
         }
+
     }
 
     @Override
